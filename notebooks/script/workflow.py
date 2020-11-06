966dccf3805fee0d0586f261c63a463593b65c19
Jay Qi <jayqi@users.noreply.github.com>
2020-11-06 12:32:25 -0500

    Update workflow

--- notebooks/script/workflow.py
+++ notebooks/script/workflow.py
@@ -13,10 +13,23 @@ class Workflow:
 workflow = Workflow(
     participants = [
         "Boromir",
+        "Frodo",
+        "Gandalf",
+        "Aragorn",
+        "Legolas",
+        "Gimli",
     ],
     steps = [
         "go_to_gondor",
-        "use_the_ring",
+        "walk_into_mordor",
+        "go_to_mount_doom",
+        "cast_ring_back_into_the_fiery_chasm_from_whence_it_came"
     ]
 )
+
+
+workflow.count_participants()
+
+
+
 
